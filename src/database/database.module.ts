b3e811199c4d7b6ee2fb4503dd7a3064f670c9b8
Joel Boada Srv Worknet <jmboada30@gmail.com>
2021-10-26 01:53:15 -0500

    se agrego typeOrm al proyecto

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -2,12 +2,26 @@ import { Module, Global } from '@nestjs/common';
 import { Client } from 'pg';
 import config from 'src/config';
 import { ConfigType } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 const API_KEY = '12345634';
 const API_KEY_PROD = 'PROD1212121SA';
 
 @Global()
 @Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [config.KEY],
+      useFactory: (configService: ConfigType<typeof config>) => {
+        const { user, ...postgres } = configService.postgres;
+        return {
+          type: 'postgres',
+          ...postgres,
+          username: user,
+        };
+      },
+    }),
+  ],
   providers: [
     {
       provide: 'API_KEY',
@@ -23,6 +37,6 @@ const API_KEY_PROD = 'PROD1212121SA';
       inject: [config.KEY],
     },
   ],
-  exports: ['API_KEY', 'PG'],
+  exports: ['API_KEY', 'PG', TypeOrmModule],
 })
 export class DatabaseModule {}
