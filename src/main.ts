5e3b8c7344813c51cefa116d30652e5623d725fa
Joel Boada Srv Worknet <jmboada30@gmail.com>
2021-10-29 06:22:22 -0500

    ahora podemos serializar con class transformer

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
-import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
+import { NestFactory, Reflector } from '@nestjs/core';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // LA SIGUIENTE LINEA ACTIVA LAS VALIDACIONES
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -13,6 +15,10 @@ async function bootstrap() {
     }),
   );
 
+  // LA SIG LINEA ACTIVA LA SERIALIZACION
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  // LAS SIGS LINEA ACTIVA LA AUTODOCUMENTACION CON SWAGGER
   const config = new DocumentBuilder()
     .setTitle('API')
     .setDescription('PLATZI STORE')
@@ -21,7 +27,9 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
+  // ACTIVAMOS LOS CORS
   app.enableCors();
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
--- src/users/entities/order.entity.ts
+++ src/users/entities/order.entity.ts
@@ -6,8 +6,10 @@ import {
   OneToMany,
   PrimaryGeneratedColumn,
 } from 'typeorm';
-import { Customer } from './customer.entity';
+import { Exclude, Expose } from 'class-transformer';
+
 import { BaseDateTime } from '../../common/entities/base-date.entity';
+import { Customer } from './customer.entity';
 import { OrderItem } from './order-item.entity';
 
 @Entity({ name: 'orders' })
@@ -25,6 +27,33 @@ export class Order {
   @JoinColumn({ name: 'customer_id' })
   customer: Customer;
 
+  // Para usar Exclude o Expose de class-transformer se debe configurar main.ts
+  // Podemos modificar la respuesta q enviaremos al frontend en esta etapa.
+  @Exclude()
   @OneToMany(() => OrderItem, (items) => items.order)
   items: OrderItem[];
+
+  @Expose()
+  get products() {
+    if (!this.items?.length) return [];
+
+    return this.items
+      .filter((item) => !!item)
+      .map((item) => ({
+        ...item.product,
+        quantity: item.quantity,
+        itemId: item.id,
+      }));
+  }
+
+  @Expose()
+  get priceTotal() {
+    if (!this.items?.length) return 0;
+    return this.items
+      .filter((item) => !!item)
+      .reduce((total, acu) => {
+        const totalItem = acu.product.price * acu.quantity;
+        return total + totalItem;
+      }, 0);
+  }
 }
