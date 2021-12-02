4aa5875175dfae08ca4e35ca8ff2907eccb7f340
Joel M. Boada V <jmboada30@gmail.com>
2021-12-02 01:55:46 -0500

    token por variable de entorno y en diferentes ambientes NODE_ENV=prod

--- src/auth/guards/api-key.guard.ts
+++ src/auth/guards/api-key.guard.ts
@@ -1,18 +1,26 @@
-import { Reflector } from '@nestjs/core';
 import {
   CanActivate,
   ExecutionContext,
+  Inject,
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import { Reflector } from '@nestjs/core';
+
 import { Request } from 'express';
 import { Observable } from 'rxjs';
+
+import config from 'src/config';
 
 import { IS_PUBLIC_KEY } from './../decorators/is-public.decorator';
 
 @Injectable()
 export class ApiKeyGuard implements CanActivate {
-  constructor(private reflector: Reflector) {}
+  constructor(
+    private reflector: Reflector,
+    @Inject(config.KEY) private configSrv: ConfigType<typeof config>,
+  ) {}
 
   canActivate(
     context: ExecutionContext,
@@ -24,7 +32,7 @@ export class ApiKeyGuard implements CanActivate {
     // Para obtener el request y evaluar lo que necesitemos
     const request = context.switchToHttp().getRequest<Request>();
     const token = request.header('Auth');
-    const isAuth = token === '1234';
+    const isAuth = token === this.configSrv.apiKey;
     if (!isAuth) {
       throw new UnauthorizedException(
         'Mensaje Personalizado para los casos donde no exista autorizacion',
